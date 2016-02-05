0209afea737f3b111d5f24bc666610afdc9e4d83
alexycruz1 <xytojr@gmail.com>
2016-02-05 16:42:17 -0600

    Finalizado

--- Lab3_progra3.cpp
+++ Lab3_progra3.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <iomanip>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::setw;
 
 void Arreglar_Score(int[], int, int);
 void llenar_arreglo(int[], int);
 
 int main(int argc, char*argv[]){
+	srand(time(NULL));
 	int opcion;
 	const int SIZE = 10;
 	int Score[SIZE];
@@ -21,18 +24,15 @@ int main(int argc, char*argv[]){
 		 << "Ingrese el ejercicio al que desea ingresar: ";
 		 cin >> opcion;
 		 cout << endl;
-
-	while(opcion != 3){
 	
 		 if (opcion == 1){
 		 		int Opcion_juego1 = 1;
 
 		 		while(Opcion_juego1 == 1){
-		 			srand(time(NULL));
 					int Intento_User;
 					int acum_intentos = 0;
 
-					int Azar = (-501 + rand()% 501);
+					int Azar = (rand()% 100 - 500);
 					cout << Azar << endl;
 
 					cout << "Ingrese un numero: " << endl;
@@ -72,16 +72,92 @@ int main(int argc, char*argv[]){
 		 	}
 
 		 	if (opcion == 2){
-		 		
-		 	}
+		 		int Row = 4;
+		 		int Col = 3;
+		 		int Matriz[Row][Col];
+		 		int J1_Ataque, J1_Defensa, J1_Velocidad, J2_Ataque, J2_Defensa, J2_Velocidad;
 
-	cout << "1.- Ejercicio 1" << endl
-		 << "2.- Ejercicio 2" << endl
-		 << "3.- Salir" << endl
-		 << "Ingrese el ejercicio al que desea ingresar: ";
-		 cin >> opcion;	 		
-		 cout << endl;
-	}//fin del while
+		 		for (int i = 0; i < Row; i++){
+		 			for (int j = 0; j < Col; j++){
+		 				if (j == 0){
+		 					int Azar = rand()% 30 + 85;
+		 					Matriz[i][j] = Azar;
+		 				}else if (j == 1){
+		 					int Azar = rand()% 25 + 50;
+		 					Matriz[i][j] = Azar;
+		 				}else if (j == 2){
+		 					int Azar = rand()% 50 + 150;
+		 					Matriz[i][j] = Azar;
+		 				}
+		 			}
+		 		}
+
+		 		cout << "        Ataque      Defensa      Velocidad" << endl;   
+		 		for (int i = 0; i < Row; i++){
+		 			for (int j = 0; j < Col; j++){
+		 				cout << setw(12) << Matriz[i][j];
+		 			}
+		 			cout << endl;
+		 		}
+
+		 		cout << endl;
+
+		 		for (int i = 0; i < 2; i++){
+		 			int Asignar_valores = (1 + rand()% 3);
+		 			int temp = Matriz[Asignar_valores][0];
+		 			if (i == 0){
+		 				J1_Ataque = temp;
+		 			}else{
+		 				J2_Ataque = temp;
+		 			}
+		 		}
+
+
+		 		for (int i = 0; i < 2; i++){
+		 			int Asignar_valores = (1 + rand()% 3);
+		 			int temp = Matriz[Asignar_valores][1];
+		 			if (i == 0){
+		 				J1_Defensa = temp;
+		 			}else{
+		 				J2_Defensa = temp;
+		 			}
+		 		}
+
+		 		for (int i = 0; i < 2; i++){
+		 			int Asignar_valores = (1 + rand()% 3);
+		 			int temp = Matriz[Asignar_valores][2];
+		 			if (i == 0){
+		 				J1_Velocidad = temp;
+		 			}else{
+		 				J2_Velocidad = temp;
+		 			}
+		 		}
+
+		 		cout << endl;
+		 		cout << "El ataque del jugador 1 es: " << J1_Ataque << endl;
+		 		cout << "El ataque del jugador 2 es: " << J2_Ataque << endl;
+		 		cout << "La defensa del jugador 1 es: " << J1_Defensa << endl;
+				cout << "La defensa del jugador 2 es: " << J2_Defensa << endl;
+		 		cout << "La velocidad del jugador 1 es: " << J1_Velocidad << endl;
+		 		cout << "La velocidad del jugador 2 es: " << J2_Velocidad << endl;
+		 		cout << endl;
+
+		 		int valor1 = J1_Ataque - J2_Defensa;
+		 		int valor2 = J2_Ataque - J1_Defensa;
+		 		if (valor1 > valor2){
+		 			cout << "El jugador 1 ha ganado" << endl;
+		 		}else if (valor1 < valor2){
+		 			cout << "El jugador 2 ha ganado" << endl;
+		 		}else if (valor1 == valor2){
+		 			if (J1_Velocidad > J2_Velocidad){
+		 				cout << "El jugador 1 ha ganado por velocidad" << endl;
+		 			}else if (J1_Velocidad < J2_Velocidad){
+		 				cout << "El jugador 2 ha ganado por velocidad" << endl;
+		 			}else if (J1_Velocidad == J2_Velocidad){
+		 				cout << "Ha habido un empate por velodidad" << endl;
+		 			}
+		 		}
+		 	}
 }
 
 void llenar_arreglo(int Score[], int SIZE){
